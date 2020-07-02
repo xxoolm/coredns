ea4989ea6db98df27e470d6fa8017ed88af5ebc1
Miek Gieben <miek@miek.nl>
2020-07-02 10:18:28 +0200

    Implement ixfr fallback and unify file and auto for transfering
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/auto/xfr.go
+++ plugin/auto/xfr.go
@@ -1,7 +1,6 @@
 package auto
 
 import (
-	"github.com/coredns/coredns/plugin/file/tree"
 	"github.com/coredns/coredns/plugin/transfer"
 
 	"github.com/miekg/dns"
@@ -16,21 +15,5 @@ func (a Auto) Transfer(zone string, serial uint32) (<-chan []dns.RR, error) {
 	if !ok || z == nil {
 		return nil, transfer.ErrNotAuthoritative
 	}
-
-	apex, err := z.ApexIfDefined()
-	if err != nil {
-		return nil, err
-	}
-
-	ch := make(chan []dns.RR)
-	go func() {
-		ch <- apex
-
-		z.Walk(func(e *tree.Elem, _ map[uint16][]dns.RR) error { ch <- e.All(); return nil })
-
-		ch <- []dns.RR{apex[0]}
-		close(ch)
-	}()
-
-	return ch, nil
+	return z.Transfer(serial)
 }
--- plugin/file/xfr.go
+++ plugin/file/xfr.go
@@ -13,6 +13,12 @@ func (f File) Transfer(zone string, serial uint32) (<-chan []dns.RR, error) {
 	if !ok || z == nil {
 		return nil, transfer.ErrNotAuthoritative
 	}
+	return z.Transfer(serial)
+}
+
+// Transfer transfers a zone with serial in the returned channel and implements IXFR fallback, by just
+// sending a single SOA record.
+func (z *Zone) Transfer(serial uint32) (<-chan []dns.RR, error) {
 	// get soa and apex
 	apex, err := z.ApexIfDefined()
 	if err != nil {
@@ -21,11 +27,17 @@ func (f File) Transfer(zone string, serial uint32) (<-chan []dns.RR, error) {
 
 	ch := make(chan []dns.RR)
 	go func() {
+		if serial != 0 && apex[0].(*dns.SOA).Serial == serial { // ixfr fallback, only send SOA
+			ch <- []dns.RR{apex[0]}
+
+			close(ch)
+			return
+		}
+
 		ch <- apex
-
 		z.Walk(func(e *tree.Elem, _ map[uint16][]dns.RR) error { ch <- e.All(); return nil })
-
 		ch <- []dns.RR{apex[0]}
+
 		close(ch)
 	}()
 
